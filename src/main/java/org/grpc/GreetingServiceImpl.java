d17c8c18e3340c617e724bce969b11ef7ab8fb84
VladislavTitarenko <vladislav@1worldonline.com>
2023-10-14 15:04:13 +0300

    Create stream

--- src/main/java/org/grpc/GreetingServiceImpl.java
+++ src/main/java/org/grpc/GreetingServiceImpl.java
@@ -5,15 +5,36 @@ import com.grpc.GreetingServiceOuterClass.HelloRequest;
 import com.grpc.GreetingServiceOuterClass.HelloResponse;
 import io.grpc.stub.StreamObserver;
 
+import java.util.stream.IntStream;
+
+import static java.lang.String.format;
+
 public class GreetingServiceImpl extends GreetingServiceImplBase {
+
+    private static final int FROM = 0;
+    private static final int TO = 20;
+    private static final int SLEEP_TIME_MILLIS = 100;
 
     @Override
     public void greeting(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
         System.out.println(request);
-        HelloResponse response = HelloResponse.newBuilder()
-                .setGreeting("Hello from server, " + request.getName()).build();
 
-        responseObserver.onNext(response);
+        IntStream.range(FROM, TO)
+                .forEach(i -> {
+                    sleep();
+                    HelloResponse response = HelloResponse.newBuilder()
+                            .setGreeting(format("Hello from server, %s, attempt: %d", request.getName(), i)).build();
+                    responseObserver.onNext(response);
+                });
+
         responseObserver.onCompleted();
+    }
+
+    private void sleep() {
+        try {
+            Thread.sleep(SLEEP_TIME_MILLIS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
